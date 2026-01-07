bb1b4d53392bbf50bac143584197e478358b7f6c
Gilberto Rincon <rincog3@medtronic.com>
2026-01-07 14:40:00 -0500

    changed reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['line'], 
-    ['junit', {outputFile: 'test-results/junit.xml'}], 
+    ['junit', {outputFile: 'test-results/results.xml'}], 
     ['html', {open: 'never', outputFolder: 'playwright-report'}]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
